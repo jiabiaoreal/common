b617cd95bcec686a62bba5d48129ea60ebe1f4f4
prist.shao <prist.shao@corp.to8to.com>
2017-06-29 16:52:43 +0800

    dial code review modify

--- dial/dial.go
+++ dial/dial.go
@@ -4,35 +4,22 @@ import (
 	"bytes"
 	"crypto/md5"
 	"encoding/json"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptrace"
-	"strconv"
 	"strings"
 	"time"
 
-	"fmt"
-
 	"golang.org/x/net/context"
-)
-
-var (
-	GetConnT   time.Time
-	GotConnT   time.Time
-	ConnStartT time.Time
-	ConnDoneT  time.Time
-	ReqStartT  time.Time
-	GotFByteT  time.Time
-	DNSStartT  time.Time
-	DNSDoneT   time.Time
 )
 
 //Drequest is a dial request messages
 type Drequest struct {
 	Header   map[string]string
 	Body     []byte
-	Method   string
+	Method   string //method must upper:GET POST PUT...
 	DailType string //http or exec or other
 	URL      string
 	Timeout  time.Duration
@@ -48,19 +35,26 @@ type Elapsed struct {
 
 //Dresponse is dialtest result
 type Dresponse struct {
-	Header  map[string]string
-	Body    string
-	Elapsed Elapsed
-	Status  bool
-	Result  string
+	//Header   map[string]string
+	//Body     string
+	Elapsed  Elapsed
+	Result   string
+	Response *http.Response
+	Status   bool
 }
 
 type Op struct {
 	client       *http.Client
 	responsetype string
-	Checkmethod  map[string]string
-	//Jmode       string //json mode [rpc/platform/other]
-	//WithResultContains, WithResultSize, WitherResultMd5sum, WithHttpClient
+	responsemin  int64
+	responsemax  int64
+	responsemd5  string
+	includeItem  string
+	excludeItem  string
+	contain      string
+	code         int
+	//Checkmethod  map[string]string
+	//WithResultContains, WithResultSize, WithResultMd5sum, WithHttpClient
 }
 
 type Option func(*Op)
@@ -69,36 +63,55 @@ func WithResponsetype(responsetype string) Option {
 	return func(op *Op) { op.responsetype = responsetype }
 }
 
-func WithClinet(client *http.Client) Option {
+func WithHttpClinet(client *http.Client) Option {
 	return func(op *Op) { op.client = client }
 }
 
-func WithCheckMethod(method map[string]string) Option {
+func WithResultCode(code int) Option {
 	return func(op *Op) {
-		op.Checkmethod = method
+		op.code = code
+	}
+}
+
+func WithResultSize(min, max int64) Option {
+	return func(op *Op) {
+		op.responsemin = min
+		op.responsemax = max
+	}
+}
+
+func WithResultInclude(item string) Option {
+	return func(op *Op) {
+		op.includeItem = item
+	}
+}
+
+func WithResultExclude(item string) Option {
+	return func(op *Op) {
+		op.excludeItem = item
+	}
+}
+
+func WithResultContains(contian string) Option {
+	return func(op *Op) {
+		op.contain = contian
+	}
+}
+
+func WithResultMd5sum(md5 string) Option {
+	return func(op *Op) {
+		op.responsemd5 = md5
 	}
 }
 
 //Dial a service, with service and interface set in the header,and this type is http
 //method:GET POST PUT DELETE..
-func Dial(dreq *Drequest, options ...Option) (dresp *Dresponse, err error) {
-	var reqJSON map[string]interface{}
-	dresp = &Dresponse{}
+func Dial(dreq *Drequest, options ...Option) (res *Dresponse, err error) {
+	res = &Dresponse{}
 
 	reopt := &Op{}
 	for _, o := range options {
 		o(reopt)
-	}
-
-	//if request Body is not json,then sucker we are,because only applicable to Java
-	err = json.Unmarshal(dreq.Body, &reqJSON)
-	if err != nil {
-		return
-	}
-
-	var buf bytes.Buffer
-	if err = json.NewEncoder(&buf).Encode(reqJSON); err != nil {
-		return
 	}
 
 	if dreq.Timeout == 0 {
@@ -111,92 +124,103 @@ func Dial(dreq *Drequest, options ...Option) (dresp *Dresponse, err error) {
 		reopt.client = &http.Client{Timeout: dreq.Timeout}
 	}
 
-	var resp *http.Response
-	var reqtime Elapsed
+	//var resp *http.Response
+	//var reqtime Elapsed
 
-	reqtime, resp, err = request(reopt.client, dreq.Method, dreq.URL, dreq.Header, &buf)
+	reqByte := bytes.NewReader(dreq.Body)
+	res, err = request(reopt.client, dreq.Method, dreq.URL, dreq.Header, reqByte)
 
 	if err != nil {
 		return
 	}
 
-	defer resp.Body.Close()
-	//start check dial result
+	defer res.Response.Body.Close()
 
+	/*r := strings.NewReader("some io.Reader stream to be read\n")
+	  	lr := io.LimitReader(resp.Body, 1024*1024)
+
+	  if _, err := io.Copy(os.Stdout, lr); err != nil {
+	      log.Fatal(err)
+	  }*/
 	var out []byte
-	out, err = ioutil.ReadAll(resp.Body)
+	out, err = ioutil.ReadAll(res.Response.Body)
 
 	var result string
-	if len(out) > 1024 {
-		result = string(out[:1024])
+	if len(out) > 1024*1024 {
+		result = string(out[:1024*1024])
 	} else {
 		result = string(out)
 	}
 
-	dresp.Header = dreq.Header
-	dresp.Body = string(dreq.Body)
-	dresp.Elapsed = reqtime
-	dresp.Result = result
-
-	respstatus := statuschk(resp.StatusCode, dreq.DailType, string(out), reopt.Checkmethod)
-	dresp.Status = respstatus
+	res.Result = result
+	res.Status = statuschk(res.Response.StatusCode, dreq.DailType, string(out), reopt)
 	return
 }
 
 // client: 对java服务不需要， 对php可能需要先认证
-func request(client *http.Client, method, url string, header map[string]string, body io.Reader) (Elapsed, *http.Response, error) {
-	requestT := Elapsed{}
+func request(client *http.Client, method, url string, header map[string]string, body io.Reader) (*Dresponse, error) {
+	//requestT := Elapsed{}
+	res := &Dresponse{}
+	var tracetime struct {
+		connstart    time.Time
+		connDone     time.Time
+		DNSstart     time.Time
+		DNSdone      time.Time
+		getconn      time.Time
+		gotconn      time.Time
+		gotfbyte     time.Time
+		requeststart time.Time
+	}
 
 	traceCtx := httptrace.WithClientTrace(context.Background(), &httptrace.ClientTrace{
 		GetConn: func(hostPort string) {
 			fmt.Printf("Prepare to get a connection for %s.\n", hostPort)
-			GetConnT = time.Now()
+			tracetime.getconn = time.Now()
 		},
 		GotConn: func(info httptrace.GotConnInfo) {
 			fmt.Printf("Got a connection: reused: %v, from the idle pool: %v.\n", info.Reused, info.WasIdle)
-			GotConnT = time.Now()
+			tracetime.gotconn = time.Now()
 		},
 		ConnectStart: func(network, addr string) {
 			fmt.Printf("Dialing... (%s:%s).\n", network, addr)
-			ConnStartT = time.Now()
+			tracetime.connstart = time.Now()
 		},
 		ConnectDone: func(network, addr string, err error) {
 			if err == nil {
 				fmt.Printf("Dial is done. (%s:%s)\n", network, addr)
-				ConnDoneT = time.Now()
-				requestT.CreateConn = time.Since(GetConnT)
+				tracetime.connDone = time.Now()
+				res.Elapsed.CreateConn = time.Since(tracetime.getconn)
 			} else {
 				fmt.Printf("Dial is done with error: %s. (%s:%s)\n", err, network, addr)
 			}
 		},
 		WroteRequest: func(info httptrace.WroteRequestInfo) {
 			if info.Err == nil {
-				//fmt.Println("Wrote a request: ok.")
-				ReqStartT = time.Now()
-				requestT.StartRequest = time.Since(GetConnT)
+				tracetime.requeststart = time.Now()
+				res.Elapsed.StartRequest = time.Since(tracetime.getconn)
 			} else {
 				fmt.Println("Wrote a request:", info.Err.Error())
 			}
 		},
 		GotFirstResponseByte: func() {
 			//fmt.Println("Got the first response byte.")
-			GotFByteT = time.Now()
-			requestT.FirstByteR = time.Since(GetConnT)
+			tracetime.gotfbyte = time.Now()
+			res.Elapsed.FirstByteR = time.Since(tracetime.getconn)
 		},
 		DNSStart: func(info httptrace.DNSStartInfo) {
 			//fmt.Println("DNS start.")
-			DNSStartT = time.Now()
+			tracetime.DNSstart = time.Now()
 		},
 		DNSDone: func(info httptrace.DNSDoneInfo) {
 			//fmt.Println("DNS done.")
-			DNSDoneT = time.Now()
-			requestT.DNStime = time.Since(DNSStartT)
+			tracetime.DNSdone = time.Now()
+			res.Elapsed.DNStime = time.Since(tracetime.DNSstart)
 		},
 	})
 
 	req, err := http.NewRequest(method, url, body)
 	if err != nil {
-		return requestT, nil, err
+		return res, err
 	}
 
 	if header != nil {
@@ -204,24 +228,76 @@ func request(client *http.Client, method, url string, header map[string]string, 
 			req.Header.Set(k, v)
 		}
 	}
-	starttime := time.Now()
+
 	req = req.WithContext(traceCtx)
+	starttime := time.Now()
 	resp, err := client.Do(req)
-	requestT.Totletime = time.Since(starttime)
-	return requestT, resp, err
+	res.Elapsed.Totletime = time.Since(starttime)
+	//res.Elapsed = requestT
+	res.Response = resp
+
+	return res, err
 }
 
 // 具体怎么检测返回的结果，可以在调用的dial的时候以option的形式指定
 // WithResultContains, WithResultSize, WitherResultMd5sum, WithHttpCode等
-func statuschk(code int, dtype, response string, checkmethod map[string]string) bool {
-	f := false
-	if dtype == "http" {
-		f = check(code, response, checkmethod)
+func statuschk(code int, dailtype, response string, op *Op) bool {
+	f := true
+	flag := true
+
+	if op.code != 0 {
+		f = chkCode(code, op.code)
+		flag = false
 	}
+
+	if op.contain != "" {
+		f = chkContains(response, op.contain)
+		flag = false
+	}
+
+	if op.excludeItem != "" {
+		f = chkInclude(response, op.excludeItem)
+		if f {
+			f = false
+		} else {
+			f = true
+		}
+		flag = false
+	}
+
+	if op.includeItem != "" {
+		f = chkInclude(response, op.includeItem)
+		flag = false
+	}
+
+	if op.responsemd5 != "" {
+		f, _ = chkMd5(response, op.responsemd5)
+		flag = false
+	}
+
+	if op.responsemin != 0 {
+		f = chkSize(response, op.responsemin, "ge")
+		flag = false
+	}
+
+	if op.responsemax != 0 {
+		f = chkSize(response, op.responsemax, "le")
+		flag = false
+	}
+
+	//if flag is true,no checkmethod give use default checkmethod
+	if flag {
+		if dailtype == "http" {
+			if code >= 400 {
+				f = false
+			}
+		}
+	}
+
 	return f
 }
 
-func check(code int, response string, checkmethod map[string]string) bool {
+/*func check(code int, response string, checkmethod map[string]string) bool {
 	s := false
 	for k, v := range checkmethod {
 		switch k {
@@ -239,9 +315,9 @@ func check(code int, response string, checkmethod map[string]string) bool {
 		case "Md5":
 			s, _ = chkMd5(response, v)
 		case "Sizege":
-			s = chkSize(response, v, "ge")
+			//s = chkSize(response, v, "ge")
 		case "Sizele":
-			s = chkSize(response, v, "le")
+			//s = chkSize(response, v, "le")
 		case "StatusCode":
 			dcode, err := strconv.Atoi(v)
 			if err == nil {
@@ -256,7 +332,7 @@ func check(code int, response string, checkmethod map[string]string) bool {
 		}
 	}
 	return s
-}
+}*/
 
 func chkMd5(response, md5sum string) (bool, error) {
 	//md5
@@ -269,19 +345,15 @@ func chkMd5(response, md5sum string) (bool, error) {
 	return false, nil
 }
 
-func chkSize(response, size, j string) bool {
+func chkSize(response string, size int64, j string) bool {
 	f := false
-	reby, err := strconv.Atoi(size)
-	if err != nil {
-		return f
-	}
 	switch j {
 	case "ge":
-		if len(response) > reby {
+		if int64(len(response)) > size {
 			f = true
 		}
 	case "le":
-		if len(response) < reby {
+		if int64(len(response)) < size {
 			f = true
 		}
 	default:
